31ff22712607bf8be1bfca269f155282bc6211f1
Roman Marasanov <Roman_Marasanov@epam.com>
2022-12-20 16:13:27 +0300

    edit V2 migration script: now hashing use bcrypt

--- src/main/resources/db.changelog/V2__create_ATM.sql
+++ src/main/resources/db.changelog/V2__create_ATM.sql
@@ -1,4 +1,7 @@
-INSERT INTO account VALUES (100, '91b4d142823f7d20c5f08df69122de43f35f057a988d9619f6d3138485c9a203', 'ATM', 'ATM', '-', 'ATM@banking.com', 'ROLE_ATM', CURRENT_TIMESTAMP);
-INSERT INTO card VALUES (100, 100, '0000000000000000', '9af15b336e6a9619928537df30b2e6a2376569fcf9d7e773eccede65606529a0', 2000000000, 'ACTIVE');
---ATM card base settings: card number is 16 zeros, pin is hashed (sha256) '0000', amount is two billion
---ATM account base settings: password is hashed (sha256) '000000'
+INSERT INTO account VALUES (100, '$2a$12$T6LFLZA/WR3Z9W/kkID3uOPNzkZON.8gYFNTWPRqtQ6q5Z3RonxB.', 'ATM1', 'ATM1', '-', 'ATM1@banking.com', 'ROLE_ATM', CURRENT_TIMESTAMP);
+INSERT INTO card VALUES (100, 100, '0000000000000000', '$2a$12$RIkk1mWP7CzD.ynNUYOiGurXk38qlF8v4DdFb.d6wlnDO3KRLvPwm', 2000000000, 'ACTIVE');
+INSERT INTO account VALUES (101, '$2a$12$T6LFLZA/WR3Z9W/kkID3uOPNzkZON.8gYFNTWPRqtQ6q5Z3RonxB.', 'ATM2', 'ATM2', '--', 'ATM2@banking.com', 'ROLE_ATM', CURRENT_TIMESTAMP);
+INSERT INTO card VALUES (101, 101, '9999999999999999', '$2a$12$RIkk1mWP7CzD.ynNUYOiGurXk38qlF8v4DdFb.d6wlnDO3KRLvPwm', 2000000000, 'ACTIVE');
+--ATM1 card base settings: card number is 16 zeros, pin is hashed (bcrypt) '0000', amount is two billion
+--ATM1 account base settings: password is hashed (bcrypt) '000000'
+--ATM2 the same but card number is 16 nines
