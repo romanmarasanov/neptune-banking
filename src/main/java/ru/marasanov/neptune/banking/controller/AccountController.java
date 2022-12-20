7100889fd60b7105f5bee8ac223e80e56e6ecfed
Roman Marasanov <Roman_Marasanov@epam.com>
2022-12-20 15:51:38 +0300

    add AccountController with get method (returns json by account id, email or phone number)

---
+++ src/main/java/ru/marasanov/neptune/banking/controller/AccountController.java
@@ -0,0 +1,35 @@
+package ru.marasanov.neptune.banking.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.marasanov.neptune.banking.model.ConverterDTO;
+import ru.marasanov.neptune.banking.model.dto.AccountDTO;
+import ru.marasanov.neptune.banking.service.AccountService;
+
+@RestController
+@RequestMapping("/api/v1/account")
+public class AccountController {
+
+    private final AccountService accountService;
+
+    @Autowired
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping
+    public AccountDTO getById(@RequestParam(required = false, name = "number") String phoneNumber,
+                              @RequestParam(required = false) String email,
+                              @RequestParam(required = false) Integer id) {
+        if (phoneNumber != null) {
+            return ConverterDTO.toAccountDTO(accountService.getByPhoneNumber(phoneNumber));
+        } else if (email != null) {
+            return ConverterDTO.toAccountDTO(accountService.getByEmail(email));
+        } else if (id != null) {
+            return ConverterDTO.toAccountDTO(accountService.getById(id));
+        } else {
+            //TODO: process no args case
+            return null;
+        }
+    }
+}
