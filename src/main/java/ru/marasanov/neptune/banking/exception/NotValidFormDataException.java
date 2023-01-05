4cf2be4dd47a09ba68baff6e58589969b3cdefb7
Roman Marasanov <Roman_Marasanov@epam.com>
2023-01-06 02:21:52 +0300

    add exception for email and phone nu,mber validators

---
+++ src/main/java/ru/marasanov/neptune/banking/exception/NotValidFormDataException.java
@@ -0,0 +1,7 @@
+package ru.marasanov.neptune.banking.exception;
+
+public class NotValidFormDataException extends Exception {
+    public NotValidFormDataException(String message) {
+        super(message);
+    }
+}
