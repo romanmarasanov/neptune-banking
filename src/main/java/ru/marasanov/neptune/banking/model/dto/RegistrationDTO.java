b27e9e357fe136a5eef02f336e3d5ce63e80f902
Roman Marasanov <Roman_Marasanov@epam.com>
2023-01-10 00:18:52 +0300

    edit RegistrationDTO: remove setter annotation due to its useless (all fields are final)

--- src/main/java/ru/marasanov/neptune/banking/model/dto/RegistrationDTO.java
+++ src/main/java/ru/marasanov/neptune/banking/model/dto/RegistrationDTO.java
@@ -2,10 +2,8 @@ package ru.marasanov.neptune.banking.model.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 @AllArgsConstructor
 public class RegistrationDTO {
     private final String firstName;
