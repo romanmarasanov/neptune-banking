a2857f5aaf8ad34b99a0b62d52d0149686e6d6f5
Roman Marasanov <Roman_Marasanov@epam.com>
2023-01-18 22:20:35 +0300

    edit RegistrationController: remove unused import

--- src/main/java/ru/marasanov/neptune/banking/controller/RegistrationController.java
+++ src/main/java/ru/marasanov/neptune/banking/controller/RegistrationController.java
@@ -1,10 +1,7 @@
 package ru.marasanov.neptune.banking.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.marasanov.neptune.banking.model.dto.RegistrationDTO;
 import ru.marasanov.neptune.banking.service.RegistrationService;
 
