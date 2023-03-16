6648fc6600eeeb3158c471d8713ea69c04ca66d5
Roman Marasanov <Roman_Marasanov@epam.com>
2023-03-16 20:12:09 +0300

    edit App and index: add basic styles and show registration at first

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import './styles/App.css';
+import RegistrationPage from "./pages/RegistrationPage";
 
 function App() {
   return (
     <div className="App">
-      <p>Hello React!</p>
+        <RegistrationPage />
     </div>
   );
 }
