044680d7785ce2d6466b7f230bfdad00769fca73
Roman Marasanov <Roman_Marasanov@epam.com>
2023-03-16 20:07:08 +0300

    add site header component and styles

---
+++ src/main/frontend/src/components/PageHeader.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "../styles/PageHeader.css"
+
+const PageHeader = () => {
+    return(
+        <div className={"PageHeader"}>
+            <h2 className={"BankName"}>Neptune Banking</h2>
+        </div>
+    );
+}
+
+export default PageHeader
